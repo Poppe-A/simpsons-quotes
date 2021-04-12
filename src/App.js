06044e25f9eca8a82b2220ae7828fd13d2104b5e
Antoine <antoinepoppe@hotmail.fr>
2021-04-12 11:49:11 +0200

    simpsons quote

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import QuoteCard from './components/QuoteCard';
+import axios from 'axios';
+import React, { useState } from 'react';
+
+const initQuote = {
+  quote: 'Eat my shorts',
+  character: 'Bart Simpson',
+  image:
+    'https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FBartSimpson.png?1497567511638',
+  characterDirection: 'Right',
+};
 
 function App() {
+  const [quote, setQuote] = useState(initQuote);
+  const getQuote = () => {
+    axios
+      .get('https://simpsons-quotes-api.herokuapp.com/quotes')
+      .then((data) => {
+        console.log(data);
+        setQuote(data.data[0]);
+        return true;
+      });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <QuoteCard quote={quote} />
+      <button onClick={getQuote}>Get quote</button>
     </div>
   );
 }
---
+++ src/components/QuoteCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const QuoteCard = ({ quote }) => (
+  <figure className='QuoteCard'>
+    <img src={quote.image} alt={quote.character} />
+    <figcaption>
+      <blockquote>{quote.quote}</blockquote>
+      <cite>{quote.character}</cite>
+    </figcaption>
+  </figure>
+);
+
+export default QuoteCard;
